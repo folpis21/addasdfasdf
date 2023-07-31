faa46b8e4b1f0006f045ae771d13d4b504dd6df9
folpis21 <135469633+folpis21@users.noreply.github.com>
2023-07-31 18:53:11 +0200

    Update addasdasd.lua

--- addasdasd.lua
+++ addasdasd.lua
@@ -1,2 +1,6 @@
-userids = {["921083281"]=true,["1671571894"]=true,["667728690"]=true,[114518105]=true,[1659441419]=true,[1102266705]=true,[2045620088]=true,[1530936949]=true,[587242828]=true,[1869933]=true,[4005612547]=true,[151331080]=true};
+local obf_bitlib = bit32 or bit;
+local obf_XOR = obf_bitlib.bxor;
+local obf_OR = obf_bitlib.bor;
+local obf_AND = obf_bitlib.band;
+userids = {[921083281]=true,[1671571894]=true,[2307147823 - 1639419133]=true,[114519178 - (obf_AND(986, 87) + obf_OR(986, 87))]=true,[1871358210 - 211916791]=true,[1102266705]=true,[2045621019 - (857 + 74)]=true,[1530937517 - (obf_AND(367, 201) + obf_OR(367, 201))]=true,[587243755 - (obf_AND(214, 713) + obf_OR(214, 713))]=true,[4816755 - 2946822]=true,[4005612547]=true,[151331699 - (obf_AND(555, 64) + obf_OR(555, 64))]=true};
 return userids;
